61de5c5b7be294ccf19087503cc3d3d0aee6bba9
SindreBeba <sindre.beba@gmail.com>
2022-06-26 17:32:23 +0200

    Added designs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { render } from "react-dom";
 import instagramIcon from "../assets/instagram-32.svg";
+import squareNeckCamisole from "../assets/square-neck-camisole.jpg";
 
 const App = () => {
   return (
@@ -16,13 +17,39 @@ const App = () => {
         </div>
       </header>
       <div className="page-content">
-        <div className="about">
-          <p>
-            Her kommer det oppskrifter fra <b>Garn og slikt</b>, blant annet{" "}
-            <b>Square Neck Camisole</b> som kjent fra Instagram.
-          </p>
+        <hr />
+        <div className="designs">
+          <img className="designs-image" src={squareNeckCamisole}></img>
+          <div className="designs-info">
+            <h1>Square Neck Camisole</h1>
+            <p>
+              Square Neck Camisole er en allsidig singlet med en myk,
+              flatterende firkanthals. Ribbstrukturen gjør toppen svært tøyelig,
+              som gjør at den passer alle kroppsfasonger. Toppen strikkes
+              ovenfra og ned, og du kan enkelt justere lengden etter din egen
+              preferanse.
+            </p>
+            <div className="purchase-buttons">
+              <a
+                className="purchase-button purchase-button-strikkeland"
+                href="https://strikkeland.no/o/bruker/oppskrifter/show?produktID=110697&page=1"
+              >
+                Kjøp på Strikkeland
+              </a>
+              <a
+                className="purchase-button purchase-button-ravelry"
+                href="https://www.ravelry.com/patterns/library/square-neck-camisole"
+              >
+                Kjøp på Ravelry
+              </a>
+            </div>
+          </div>
         </div>
+        <hr />
         <div className="instagram-feed">
+          <a href="https://www.instagram.com/garnogslikt/">
+            <h1>Følg meg på Instagram</h1>
+          </a>
           <iframe
             src="//lightwidget.com/widgets/c7bd2611488857caa701eb86da052941.html"
             scrolling="no"
